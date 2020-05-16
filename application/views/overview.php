e6ccef9c4796499787550e0c736c1be478833fcf
aderahmatn <nurdiyana.ade@gmail.com>
2020-05-16 13:50:47 +0700

    create menu dashboard

--- application/views/overview.php
+++ application/views/overview.php
@@ -59,6 +59,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+               <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
           <li class="nav-item has-treeview menu-close">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-book"></i>
